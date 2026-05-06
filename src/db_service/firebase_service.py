a30387f14ba2d9dea65ee882eb4776716bf41e24
vomisa29 <p.mendezm@uniandes.edu.co>
2026-05-05 20:35:00 -0500

    Back Sarasvati

--- src/db_service/firebase_service.py
+++ src/db_service/firebase_service.py
@@ -1,6 +1,8 @@
 from db_service.firebase_class import firebaseDB
+import os
 
-path = "./src/secrets/credentials.json"
+path = os.getenv("GOOGLE_CREDENTIALS")
+# path = "./src/secrets/credentials.json"
 url_database="https://back-sarasvati-default-rtdb.firebaseio.com/"
 
 class firebaseService:
